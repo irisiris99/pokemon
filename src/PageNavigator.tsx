31fc4591c75963e47b6efde29fa8f57ef56ddfb6
irisiris99 <cjg03117@gmail.com>
2022-08-06 19:02:30 +0800

    asd21

--- src/PageNavigator.tsx
+++ src/PageNavigator.tsx
@@ -1,16 +1,13 @@
 import { Routes, Route } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
 import PokemonDetail from './Detail/PokemonDetail'
 import PokeCardList from './List/PokeCardList'
 
 const PageNavigator = () => {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<PokeCardList />}/>
-        <Route path="/pokemon/:name" element={<PokemonDetail />}/>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<PokeCardList />}/>
+      <Route path="/pokemon/:name" element={<PokemonDetail />}/>
+    </Routes>
   )
 }
 
