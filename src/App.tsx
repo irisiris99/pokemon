dc972d10f30e9419e1389dd260c78fdb32f0f36d
irisiris99 <cjg03117@gmail.com>
2022-08-22 22:07:27 +0800

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import {  BrowserRouter, HashRouter, HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, HashRouter as Router, Route, Routes } from 'react-router-dom';
 import PageHeader from './Common/PageHeader';
 import PokemonDetail from './Detail/PokemonDetail'
 import PokeCardList from './List/PokeCardList'
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <PageHeader />
       <Routes>
-        <Route path="/" element={<PokeCardList />}/>
-        <Route path="/pokemon/:name" element={<PokemonDetail />}/>
+        <Route path="/" element={<PokeCardList />} />
+        <Route path="/pokemon/:name" element={<PokemonDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
